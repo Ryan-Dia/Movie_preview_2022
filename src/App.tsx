e5ba0eef876a9f97b2b87bf6464b180b35a15ec0
ryan-dia <cjfdnjs158@naver.com>
2023-03-07 04:36:16 +0900

    refactor: 불필요한 코드 삭제 및 tvId가 있을 때만 fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PreHome from "./Routes/PreHome";
-import Home from "./Routes/Home";
-import Tv from "./Routes/Tv";
-import Movie from "./Routes/Movie";
-import Join from "./Routes/Join";
-import About from "./Routes/About";
-import Search from "./Routes/Search";
-import Header from "./Components/Header";
-import Error from "./Components/error/Error";
-import { ReactQueryDevtools } from "react-query/devtools";
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PreHome from './Routes/PreHome';
+import Home from './Routes/Home';
+import Tv from './Routes/Tv';
+import Movie from './Routes/Movie';
+import Join from './Routes/Join';
+import About from './Routes/About';
+import Search from './Routes/Search';
+import Header from './Components/Header';
+import Error from './Components/error/Error';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 function App() {
   return (
     <>
-        <Router basename={process.env.PUBLIC_URL}>
-          <Header />
-          <Routes>
-            <Route path="/" element={<PreHome />}></Route>
-            <Route path="home" element={<Home />}>
-              <Route path="movies/:movieId" element={<Home/>} />
-            </Route>
-            <Route path="tv" element={<Tv />}>
-              <Route path="tvs/:tvId" element={<Tv/>} />
-            </Route>
-            <Route path="movie" element={<Movie />}></Route>
-            <Route path="search" element={<Search />}></Route>
-            <Route path="join" element={<Join />}></Route>
-            <Route path="about" element={<About />}></Route>
-            <Route path={"*"} element={<Error/>}></Route>
-          </Routes>
-        </Router>
-        <ReactQueryDevtools initialIsOpen={true} />
-        
+      <Router basename={process.env.PUBLIC_URL}>
+        <Header />
+        <Routes>
+          <Route path='/' element={<PreHome />}></Route>
+          <Route path='home' element={<Home />}>
+            <Route path='movies/:movieId' element={<Home />} />
+          </Route>
+          <Route path='tv' element={<Tv />}>
+            <Route path='tvs/:tvId' element={<Tv />} />
+          </Route>
+          <Route path='movie' element={<Movie />}></Route>
+          <Route path='search' element={<Search />}></Route>
+          <Route path='join' element={<Join />}></Route>
+          <Route path='about' element={<About />}></Route>
+          <Route path={'*'} element={<Error />}></Route>
+        </Routes>
+      </Router>
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }
--- src/Routes/Tv.tsx
+++ src/Routes/Tv.tsx
@@ -1,28 +1,22 @@
-import { motion, AnimatePresence, useViewportScroll } from "framer-motion";
-import { useEffect, useState } from "react";
-import { useQuery } from "react-query";
-import { useNavigate, useMatch } from "react-router-dom";
-import styled from "styled-components";
-import {
-  getTvPopular,
-  getTvVideo,
-  IGetTvResult,
-  IGetTvVideo,
-  ITvTopRated,
-} from "../api";
-import { makeImagePath } from "../utils";
-import MainTrailer from "./MainTrailer";
-import ErrorBoundary from "./errorboundary";
+import { motion, AnimatePresence, useViewportScroll } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
+import { useNavigate, useMatch } from 'react-router-dom';
+import styled from 'styled-components';
+import { getTvPopular, getTvVideo, IGetTvResult, IGetTvVideo, ITvTopRated } from '../api';
+import { makeImagePath } from '../utils';
+import MainTrailer from './MainTrailer';
+import ErrorBoundary from './errorboundary';
 
-import BoxVideo from "./boxVideo";
-import BigBoxVideo from "./bigBoxVideo";
-import BigBoxInfo from "../Components/BigBoxInfo";
-import "../fonts/fonts.css";
-import {client} from "../index"
-import TvBoxVideo from "./TvBoxVideo";
-import TvTopRated from "../Components/Tv/TvTopRated";
-import TvOnAir from "../Components/Tv/TvOnAir";
-import TvBigBoxInfo from "../Components/Tv/TvBigBoxInfo";
+import BoxVideo from './boxVideo';
+import BigBoxVideo from './bigBoxVideo';
+import BigBoxInfo from '../Components/BigBoxInfo';
+import '../fonts/fonts.css';
+import { client } from '../index';
+import TvBoxVideo from './TvBoxVideo';
+import TvTopRated from '../Components/Tv/TvTopRated';
+import TvOnAir from '../Components/Tv/TvOnAir';
+import TvBigBoxInfo from '../Components/Tv/TvBigBoxInfo';
 
 const Wrapper = styled.div`
   background: transparent;
@@ -36,8 +30,6 @@ const Loader = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  
-  }
 `;
 
 const Banner = styled.div`
@@ -61,21 +53,18 @@ const Title = styled(motion.h2)`
   font-size: 85px;
   font-weight: bold;
   margin-bottom: 20px;
-  font-family: "twaysky";
+  font-family: 'twaysky';
 `;
 const Overview = styled(motion.p)`
   font-size: 24px;
   width: 31%;
-  font-family: "GowunDodum-Regular";
+  font-family: 'GowunDodum-Regular';
 `;
 
 const Slider = styled.div`
-
   position: relative;
   top: -15vh;
-  z-index:2;
-  
-  }
+  z-index: 2;
 `;
 
 const Row = styled(motion.div)`
@@ -112,8 +101,8 @@ const boxVariants = {
   hover: {
     y: -80,
     scale: 1.3,
-    zIndex:5,
-    transition: { delay: 1, type: "tween", duration: 0.2 },
+    zIndex: 5,
+    transition: { delay: 1, type: 'tween', duration: 0.2 },
   },
 };
 
@@ -125,7 +114,7 @@ const arrowVariants = {
     x: 8,
     y: 10,
     scale: 1.1,
-    ransition: { delay: 0.2, type: "tween", duration: 0.3 },
+    ransition: { delay: 0.2, type: 'tween', duration: 0.3 },
   },
 };
 
@@ -160,7 +149,7 @@ const infoVariants = {
   },
   hover: {
     opacity: 1,
-    transition: { delay: 1.4, type: "tween", duration: 0.8 },
+    transition: { delay: 1.4, type: 'tween', duration: 0.8 },
   },
 };
 
@@ -186,16 +175,13 @@ const Overlay = styled(motion.div)`
 
 const offset = 6;
 
-
 function Tv() {
-
   const [onHover, setOnHover] = useState(false);
   const onHovers = () => {
-    setTimeout(()=>setOnHover(true), 1000)
+    setTimeout(() => setOnHover(true), 1000);
   };
   const outHovers = () => {
-    setTimeout(()=>setOnHover(false), 1000)
-    
+    setTimeout(() => setOnHover(false), 1000);
   };
   const [bigBox, setBigBox] = useState(false);
   const clickBigBox = () => {
@@ -206,23 +192,24 @@ function Tv() {
   };
 
   const navigate = useNavigate();
-  const bigTvMatch = useMatch("/tv/tvs/:tvId");
+  const bigTvMatch = useMatch('/tv/tvs/:tvId');
   const { scrollY } = useViewportScroll();
   const { data, isLoading } = useQuery<IGetTvResult>(
-    ["TV", "Popular"],
+    ['TV', 'Popular'],
     async () => await getTvPopular()
   );
   const tvId = data?.results[0].id;
   const tvBase = data?.results[0];
 
   const { data: videoData, isLoading: videoLoading } = useQuery<IGetTvVideo>(
-    ["TV_video", tvId],
-    () => getTvVideo(tvId)
+    ['TV_video', tvId],
+    () => getTvVideo(tvId),
+    { enabled: !!tvId }
   );
 
-  let videDataResource = videoData?.results.find((x) => x.type === "Trailer");
+  let videDataResource = videoData?.results.find((x) => x.type === 'Trailer');
   if (!videDataResource) {
-    videDataResource = videoData?.results.find((x) => x.type === "Teaser");
+    videDataResource = videoData?.results.find((x) => x.type === 'Teaser');
   }
   const bannerVideo = videDataResource?.key;
 
@@ -254,32 +241,25 @@ function Tv() {
   const onBoxClicked = (tvId: number) => {
     navigate(`/tv/tvs/${tvId}`);
   };
-  const onOverlayClick = () => navigate("/tv");
+  const onOverlayClick = () => navigate('/tv');
 
-  const topRatedData = client.getQueryData<IGetTvResult>(["TV","TopLated"])
-  const onAirData = client.getQueryData<IGetTvResult>(["TV","OnAir"])
+  const topRatedData = client.getQueryData<IGetTvResult>(['TV', 'TopLated']);
+  const onAirData = client.getQueryData<IGetTvResult>(['TV', 'OnAir']);
   let clickedTv =
     bigTvMatch?.params.tvId &&
-    data?.results.find(
-      (tv) => tv.id + "" === bigTvMatch.params.tvId
-    );
+    data?.results.find((tv) => tv.id + '' === bigTvMatch.params.tvId);
 
-  if(!clickedTv){
-    clickedTv = bigTvMatch?.params.tvId &&
-    topRatedData?.results.find(
-      (tv) => tv.id + "" === bigTvMatch.params.tvId
-    );
+  if (!clickedTv) {
+    clickedTv =
+      bigTvMatch?.params.tvId &&
+      topRatedData?.results.find((tv) => tv.id + '' === bigTvMatch.params.tvId);
   }
-  if(!clickedTv){
-    clickedTv = bigTvMatch?.params.tvId &&
-    onAirData?.results.find(
-      (tv) => tv.id + "" === bigTvMatch.params.tvId
-    );
-  }   
+  if (!clickedTv) {
+    clickedTv =
+      bigTvMatch?.params.tvId &&
+      onAirData?.results.find((tv) => tv.id + '' === bigTvMatch.params.tvId);
+  }
 
-
- 
-  
   const [hovers, setHovers] = useState(false);
   const [nums, setnums] = useState(0);
   const hoverBox = () => {
@@ -291,18 +271,13 @@ function Tv() {
   const hoverOut = () => {
     setHovers(false);
   };
- 
-  
+
   useEffect(() => {
-    const tvDelete = data?.results.findIndex(x=> x.poster_path === null )!
-    if(tvDelete !== -1){
-      data?.results.splice(tvDelete, 1)
+    const tvDelete = data?.results.findIndex((x) => x.poster_path === null)!;
+    if (tvDelete !== -1) {
+      data?.results.splice(tvDelete, 1);
     }
-    
- 
-}, [data]);
-  
-  
+  }, [data]);
 
   return (
     <Wrapper>
@@ -312,164 +287,168 @@ function Tv() {
         <>
           <ErrorBoundary>
             <Banner>
-              {videoLoading ? (<Loader>Loading...</Loader>) : (
+              {videoLoading ? (
+                <Loader>Loading...</Loader>
+              ) : (
                 <MainTrailer
-                bannerVideo={bannerVideo}
-                videoLoading={videoLoading}
-                onHover={onHover}
-                bigBox={bigBox}
-              ></MainTrailer>
+                  bannerVideo={bannerVideo}
+                  videoLoading={videoLoading}
+                  onHover={onHover}
+                  bigBox={bigBox}
+                ></MainTrailer>
               )}
-              
+
               <MainBox
-                transition={{ delay: 7, type: "tween", duration: 3 }}
-                animate={{ top: "650px" }}
+                transition={{ delay: 7, type: 'tween', duration: 3 }}
+                animate={{ top: '650px' }}
               >
                 <Title
-                  transition={{ delay: 7, type: "tween", duration: 3 }}
-                  animate={{ fontSize: "50px" }}
+                  transition={{ delay: 7, type: 'tween', duration: 3 }}
+                  animate={{ fontSize: '50px' }}
                 >
                   {tvBase?.name}
                 </Title>
                 <Overview
-                  transition={{ delay: 7, type: "tween", duration: 6 }}
-                  animate={{ display: "none" }}
+                  transition={{ delay: 7, type: 'tween', duration: 6 }}
+                  animate={{ display: 'none' }}
                 >
-                  {tvBase?.overview.slice(0, 120) + "..."}
+                  {tvBase?.overview.slice(0, 120) + '...'}
                 </Overview>
               </MainBox>
             </Banner>
-          
-          <Slider>
-            <motion.svg
-              whileHover="hover"
-              initial="normal"
-              variants={arrowVariants}
-              fill="currentColor"
-              viewBox="0 0 256 512"
-              style={{
-                position: "absolute",
-                zIndex: "2",
-                width: "50",
-                height: "60",
-                bottom: "-300",
-              }}
-              xmlns="http://www.w3.org/2000/svg"
-              transition={{ type: "tween" }}
-              onClick={decreaseIndex}
-            >
-              <path d="M192 448c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l137.4 137.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448z"></path>
-            </motion.svg>
-            <AnimatePresence
-              initial={false}
-              onExitComplete={toggleLeaving}
-              custom={back}
-            >
-              <Row
-                variants={rowVariants}
-                initial="hidden"
-                animate="visible"
-                exit="exit"
-                key={index}
-                transition={{ type: "tween", duration: 1 }}
+
+            <Slider>
+              <motion.svg
+                whileHover='hover'
+                initial='normal'
+                variants={arrowVariants}
+                fill='currentColor'
+                viewBox='0 0 256 512'
+                style={{
+                  position: 'absolute',
+                  zIndex: '2',
+                  width: '50',
+                  height: '60',
+                  bottom: '-300',
+                }}
+                xmlns='http://www.w3.org/2000/svg'
+                transition={{ type: 'tween' }}
+                onClick={decreaseIndex}
+              >
+                <path d='M192 448c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l137.4 137.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448z'></path>
+              </motion.svg>
+              <AnimatePresence
+                initial={false}
+                onExitComplete={toggleLeaving}
                 custom={back}
               >
-                {data?.results
-                  .slice(1)
-                  .slice(offset * index, offset * index + offset)
-                  .map((tv) => ( 
-                    <Box
-                      layoutId={tv.id + ""}
-                      key={tv.id}
-                      whileHover="hover"
-                      onHoverStart={onHovers}
-                      onHoverEnd={outHovers}
-                      initial="normal"
-                      variants={boxVariants}
-                      onClick={() => {
-                        onBoxClicked(tv.id);
-                        clickBigBox();
-                      }}
-                      transition={{ type: "tween" }}
-                      bgphoto={tv.poster_path?  makeImagePath(tv.poster_path!, "w500") 
-                      : makeImagePath(tv.backdrop_path!, "w500")}
-                    >
-                      <Info
-                        initial="normal"
-                        whileHover="hover"
-                        transition={{ type: "tween" }}
-                        key={tv.id + "bbq"}
-                        variants={infoVariants}
-                        onMouseEnter={() => {
-                          hoverNum(tv.id);
-                          hoverBox();
-                        }}
-                        onMouseLeave={hoverOut}
-                      >
-                        {hovers ? (
-                          nums === tv.id ? (
-                            <TvBoxVideo
-                              id={tv.id}
-                              gre={tv.genre_ids}
-                              title={tv.name}
-                              vote={tv.vote_average}
-                            ></TvBoxVideo>
-                          ) : null
-                        ) : null}
-                      </Info>
-                    </Box>
-                  ))}
-              </Row>
-            </AnimatePresence>
-            <motion.svg
-              whileHover="hover"
-              initial="normal"
-              variants={arrowVariants}
-              fill="currentColor"
-              viewBox="0 0 256 512"
-              style={{
-                position: "absolute",
-                zIndex: "2",
-                width: "50",
-                height: "60",
-                bottom: "-300",
-                right: "2",
-              }}
-              xmlns="http://www.w3.org/2000/svg"
-              transition={{ type: "tween" }}
-              onClick={incraseIndex}
-            >
-              <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
-            </motion.svg>
-          </Slider>
-          <TvTopRated></TvTopRated>
-          <TvOnAir></TvOnAir>    
-          <AnimatePresence>
-            {bigTvMatch ? (
-              <>
-                <Overlay
-                  onClick={() => {
-                    onOverlayClick();
-                    clickOverlay();
-                  }}
-                  exit={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                />
-                <BigMovie
-                  style={{ top: scrollY.get() + 100, zIndex: "101" }}
-                  layoutId={bigTvMatch.params.tvId}
+                <Row
+                  variants={rowVariants}
+                  initial='hidden'
+                  animate='visible'
+                  exit='exit'
+                  key={index}
+                  transition={{ type: 'tween', duration: 1 }}
+                  custom={back}
                 >
-                  {clickedTv && (
-                    <>
-                      <BigBoxVideo id={clickedTv.id} gre="tv"></BigBoxVideo> 
-                      <TvBigBoxInfo base={clickedTv} ></TvBigBoxInfo>
-                      
-                    </>
-                  )}
-                </BigMovie>
-              </>
-            ) : null}
-          </AnimatePresence>
+                  {data?.results
+                    .slice(1)
+                    .slice(offset * index, offset * index + offset)
+                    .map((tv) => (
+                      <Box
+                        layoutId={tv.id + ''}
+                        key={tv.id}
+                        whileHover='hover'
+                        onHoverStart={onHovers}
+                        onHoverEnd={outHovers}
+                        initial='normal'
+                        variants={boxVariants}
+                        onClick={() => {
+                          onBoxClicked(tv.id);
+                          clickBigBox();
+                        }}
+                        transition={{ type: 'tween' }}
+                        bgphoto={
+                          tv.poster_path
+                            ? makeImagePath(tv.poster_path!, 'w500')
+                            : makeImagePath(tv.backdrop_path!, 'w500')
+                        }
+                      >
+                        <Info
+                          initial='normal'
+                          whileHover='hover'
+                          transition={{ type: 'tween' }}
+                          key={tv.id + 'bbq'}
+                          variants={infoVariants}
+                          onMouseEnter={() => {
+                            hoverNum(tv.id);
+                            hoverBox();
+                          }}
+                          onMouseLeave={hoverOut}
+                        >
+                          {hovers ? (
+                            nums === tv.id ? (
+                              <TvBoxVideo
+                                id={tv.id}
+                                gre={tv.genre_ids}
+                                title={tv.name}
+                                vote={tv.vote_average}
+                              ></TvBoxVideo>
+                            ) : null
+                          ) : null}
+                        </Info>
+                      </Box>
+                    ))}
+                </Row>
+              </AnimatePresence>
+              <motion.svg
+                whileHover='hover'
+                initial='normal'
+                variants={arrowVariants}
+                fill='currentColor'
+                viewBox='0 0 256 512'
+                style={{
+                  position: 'absolute',
+                  zIndex: '2',
+                  width: '50',
+                  height: '60',
+                  bottom: '-300',
+                  right: '2',
+                }}
+                xmlns='http://www.w3.org/2000/svg'
+                transition={{ type: 'tween' }}
+                onClick={incraseIndex}
+              >
+                <path d='M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z' />
+              </motion.svg>
+            </Slider>
+            <TvTopRated></TvTopRated>
+            <TvOnAir></TvOnAir>
+            <AnimatePresence>
+              {bigTvMatch ? (
+                <>
+                  <Overlay
+                    onClick={() => {
+                      onOverlayClick();
+                      clickOverlay();
+                    }}
+                    exit={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                  />
+                  <BigMovie
+                    style={{ top: scrollY.get() + 100, zIndex: '101' }}
+                    layoutId={bigTvMatch.params.tvId}
+                  >
+                    {clickedTv && (
+                      <>
+                        <BigBoxVideo id={clickedTv.id} gre='tv'></BigBoxVideo>
+                        <TvBigBoxInfo base={clickedTv}></TvBigBoxInfo>
+                      </>
+                    )}
+                  </BigMovie>
+                </>
+              ) : null}
+            </AnimatePresence>
           </ErrorBoundary>
         </>
       )}
